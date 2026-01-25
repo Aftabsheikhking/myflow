033e40e42f64a2484c8cf093dc65ebffd1908a2a
Aftabsheikhking <aftabsheikking@gmail.com>
2026-01-25 14:57:58 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo "Installing samar tool..."
+sleep 1
+
+chmod +x samar
+
+if [ -d "$PREFIX/bin" ]; then
+  mv emoji $PREFIX/bin/
+else
+  echo "PREFIX/bin not found"
+  exit 1
+fi
+
+echo ""
+echo "Install complete ✅"
+echo "Now type: samar"
